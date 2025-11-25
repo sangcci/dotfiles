fcfd2e6828820a197e7010b07cbd5effa2db7524
sangcci <pshsh910@gmail.com>
2025-11-26 00:03:31 +0100

    fix: add transparency in neovim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -25,3 +25,12 @@ require("base16-colorscheme").with_config({
 	dapui = true,
 })
 vim.cmd.colorscheme("base16-black-metal-khold")
+
+-- Set transparency AFTER loading colorscheme
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
