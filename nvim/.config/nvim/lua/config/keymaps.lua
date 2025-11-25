9b4358f717e04f92641dee9dd26dce5ec8638f87
sangcci <pshsh910@gmail.com>
2025-11-25 19:19:24 +0100

    fix: change most of keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -35,57 +35,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local opts = { buffer = args.buf }
 
-		vim.keymap.set(
-			"n",
-			"<leader>ch",
-			vim.lsp.buf.hover,
-			vim.tbl_extend("force", opts, { desc = "[H]over Documentation" })
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>cd",
-			vim.lsp.buf.definition,
-			vim.tbl_extend("force", opts, { desc = "[D]efinition" })
-		)
-		vim.keymap.set(
-			{ "n", "v" },
-			"<leader>ca",
-			vim.lsp.buf.code_action,
-			vim.tbl_extend("force", opts, { desc = "[A]ctions" })
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>cr",
-			require("telescope.builtin").lsp_references,
-			vim.tbl_extend("force", opts, { desc = "[R]eferences" })
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>ci",
-			require("telescope.builtin").lsp_implementations,
-			vim.tbl_extend("force", opts, { desc = "[I]mplementations" })
-		)
-		vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "[R]ename" }))
-		-- using conform plugins
-		vim.keymap.set("n", "<leader>cf", function()
-			require("conform").format({ bufnr = args.buf, async = true, lsp_fallback = true })
-		end, vim.tbl_extend("force", opts, { desc = "re[F]ormat" }))
+		-- open diagnositcs on float window
+		vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
-		-- Diagnostics
-		vim.keymap.set(
-			"n",
-			"<leader>ce",
-			vim.diagnostic.open_float,
-			vim.tbl_extend("force", opts, { desc = "Show diagnostics [E]" })
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>cq",
-			vim.diagnostic.setloclist,
-			vim.tbl_extend("force", opts, { desc = "Diagnostics to loclist [Q]" })
-		)
+		-- using conform plugins
+		vim.keymap.set("n", "<leader>r", function()
+			require("conform").format({ bufnr = args.buf, async = true, lsp_fallback = true })
+		end, vim.tbl_extend("force", opts, { desc = "format" }))
 	end,
 })
+
 -- [D]ebug
 local dap = require("dap")
 local dapui = require("dapui")
