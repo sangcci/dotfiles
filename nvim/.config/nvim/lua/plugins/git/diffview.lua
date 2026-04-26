031513da31735bd3964ecf65240aa053b00c3b98
sangcci <pshsh910@gmail.com>
2026-04-26 14:43:03 +0900

    feat(diffview): 토글 키맵 및 q로 창 닫기 추가
    <leader>go를 열기/닫기 토글로 변경.
    BufWinEnter에서 q를 DiffviewClose로 abbrev 처리.

--- nvim/.config/nvim/lua/plugins/git/diffview.lua
+++ nvim/.config/nvim/lua/plugins/git/diffview.lua
@@ -21,7 +21,26 @@ require("diffview").setup({
 	},
 })
 
-vim.keymap.set("n", "<leader>go", ":DiffviewOpen<CR>", { desc = "Diffview Open" })
+local function diffview_is_open()
+	local ok, lib = pcall(require, "diffview.lib")
+	return ok and lib.get_current_view() ~= nil
+end
+
+vim.keymap.set("n", "<leader>go", function()
+	if diffview_is_open() then
+		vim.cmd("DiffviewClose")
+	else
+		vim.cmd("DiffviewOpen")
+	end
+end, { desc = "Diffview Toggle" })
 vim.keymap.set("n", "<leader>gc", ":DiffviewClose<CR>", { desc = "Diffview Close" })
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	callback = function()
+		if diffview_is_open() then
+			vim.cmd([[cnoreabbrev <expr> <buffer> q (getcmdtype() == ':' && getcmdline() ==# 'q') ? 'DiffviewClose' : 'q']])
+		end
+	end,
+})
 vim.keymap.set("n", "<leader>gh", ":DiffviewFileHistory<CR>", { desc = "Diffview File History" })
 vim.keymap.set("n", "<leader>gf", ":DiffviewFileHistory %<CR>", { desc = "Diffview File History %" })
