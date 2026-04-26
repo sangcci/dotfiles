18444b9f9569ef8d6545141f69647dcb927ba60e
sangcci <pshsh910@gmail.com>
2026-04-26 14:53:06 +0900

    feat(dropbar): neo-tree/diffview에서 비활성화
    bar.enable 조건 추가로 특정 파일타입과 diff 창에서 dropbar 숨김.

--- nvim/.config/nvim/lua/plugins/util/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/util/dropbar.lua
@@ -2,7 +2,31 @@ vim.pack.add({
 	{ src = "https://github.com/Bekaboo/dropbar.nvim" },
 })
 
-require("dropbar").setup()
+local excluded_filetypes = {
+	["neo-tree"] = true,
+	["DiffviewFiles"] = true,
+	["DiffviewFileHistory"] = true,
+	["DiffviewFileHistoryPanel"] = true,
+}
+
+require("dropbar").setup({
+	bar = {
+		enable = function(buf, win, _)
+			if not buf or not win then
+				return false
+			end
+			if not vim.api.nvim_buf_is_valid(buf) or not vim.api.nvim_win_is_valid(win) then
+				return false
+			end
+			if excluded_filetypes[vim.bo[buf].filetype] then
+				return false
+			end
+			return vim.bo[buf].buftype == ""
+				and vim.api.nvim_buf_get_name(buf) ~= ""
+				and not vim.wo[win].diff
+		end,
+	},
+})
 
 local dropbar_api = require("dropbar.api")
 vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
