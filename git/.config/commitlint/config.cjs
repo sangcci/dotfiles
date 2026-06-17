6830d78422a6fd4981b2affd819765cf1a1065a0
sangcci <pshsh910@gmail.com>
2026-06-17 18:24:56 +0900

    chore(test): commitlint 본문 길이 제한 완화

--- git/.config/commitlint/config.cjs
+++ git/.config/commitlint/config.cjs
@@ -1,16 +1,28 @@
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
   rules: {
-    'type-enum': [2, 'always', [
-      'feat', 'fix', 'hotfix', 'chore', 'refactor', 'test', 'docs', 'style', 'perf',
-    ]],
-    'type-empty': [2, 'never'],
-    'type-case': [2, 'always', 'lower-case'],
-    'scope-empty': [2, 'never'],
-    'subject-empty': [2, 'never'],
-    'header-max-length': [2, 'always', 50],
-    'subject-full-stop': [2, 'never', '.'],
-    'subject-case': [0],
-    'body-max-line-length': [2, 'always', 72],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "hotfix",
+        "chore",
+        "refactor",
+        "test",
+        "docs",
+        "style",
+        "perf",
+      ],
+    ],
+    "type-empty": [2, "never"],
+    "type-case": [2, "always", "lower-case"],
+    "scope-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "header-max-length": [2, "always", 50],
+    "subject-full-stop": [2, "never", "."],
+    "subject-case": [0],
+    "body-max-line-length": [2, "always", 100],
   },
 };
