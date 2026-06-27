342d864b70975103b7da16e59fc701abf61f2cf7
sangcci <pshsh910@gmail.com>
2026-06-27 15:17:28 +0900

    fix(test): Dropbar 배경색을 Normal과 동기화

--- nvim/.config/nvim/lua/plugins/util/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/util/dropbar.lua
@@ -28,6 +28,37 @@ require("dropbar").setup({
 	},
 })
 
+local function set_hl_bg(group, bg)
+	local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = group, link = false })
+	if not ok then
+		hl = {}
+	end
+	hl.bg = bg
+	vim.api.nvim_set_hl(0, group, hl)
+end
+
+local function set_dropbar_bg()
+	local ok, normal = pcall(vim.api.nvim_get_hl, 0, { name = "Normal", link = false })
+	local bg = ok and normal.bg or "NONE"
+	if not bg then
+		bg = "NONE"
+	end
+
+	set_hl_bg("WinBar", bg)
+	set_hl_bg("WinBarNC", bg)
+
+	for group, _ in pairs(vim.api.nvim_get_hl(0, {})) do
+		if group:match("^DropBar") then
+			set_hl_bg(group, bg)
+		end
+	end
+end
+
+set_dropbar_bg()
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = set_dropbar_bg,
+})
+
 local dropbar_api = require("dropbar.api")
 vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
 vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
