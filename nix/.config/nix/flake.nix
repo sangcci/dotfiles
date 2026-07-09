05e86969910bad021a8deacc9073ed584c3c34bd
sangcci <pshsh910@gmail.com>
2026-07-09 20:55:18 +0900

    feat(test): home-manager 입력 추가
    nix-darwin 구성에서 home-manager 모듈을 사용할 수 있도록 flake 입력과 lock을 갱신합니다.

--- nix/.config/nix/flake.nix
+++ nix/.config/nix/flake.nix
@@ -6,14 +6,18 @@
 
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, ... }:
+  outputs = inputs@{ self, nix-darwin, home-manager, ... }:
   {
     darwinConfigurations."Hyeoks-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       specialArgs = { inherit self; };
 
       modules = [
+        home-manager.darwinModules.home-manager
         ./modules/common
         ./modules/darwin
       ];
